3661c3e88743036e281c04e3fe0433a2c6137d22
Richard Jones <richard.jones@crowdcube.com>
2021-01-23 12:16:33 +0000

    Initial commit

---
+++ src/main/java/com/rj93/medium/redditoauth2/RedditOauth2Application.java
@@ -0,0 +1,13 @@
+package com.rj93.medium.redditoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rj93/medium/redditoauth2/RedditOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rj93.medium.redditoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
