1eb6f350c52d520bf6e1af38af801cbd33c77696
Richard Jones <richard.jones@crowdcube.com>
2021-01-24 15:34:36 +0000

    Part 1 - Authentication

---
+++ src/main/java/com/rj93/medium/redditoauth2/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.rj93.medium.redditoauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(
+            ServerHttpSecurity http) {
+        http
+                .authorizeExchange().anyExchange().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
